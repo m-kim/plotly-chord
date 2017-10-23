2a5168d2a81e4e3dfe30c02d5ae7b8702622e042
Mark Kim <mbk@cs.utah.edu>
2017-10-23 16:36:22 -0400

    Make plot.ly chord example into a callable function. Change some of the tuples to np.array for performance(?)

--- Chord.py
+++ Chord.py
@@ -4,20 +4,14 @@ import plotly.figure_factory as ff
 from plotly.graph_objs import *
 
 import numpy as np
+import sys
 
-matrix=np.array([[16,  3, 28,  0, 18],
-                 [18,  0, 12,  5, 29],
-                 [ 9, 11, 17, 27,  0],  
-                 [19,  0, 31, 11, 12],
-                 [23, 17, 10,  0, 34]], dtype=int)
-
-def check_data(data_matrix):
+def isSquare(data_matrix):
     L, M=data_matrix.shape
     if L!=M:
         raise ValueError('Data array must have (n,n) shape')
-    return L
 
-L=check_data(matrix)
+
 
 PI=np.pi
 
@@ -28,31 +22,25 @@ def moduloAB(x, a, b): #maps a real number onto the unit circle identified with
         y=(x-a)%(b-a)
         return y+b if y<0 else y+a
     
-def test_2PI(x):
+def bounds(x):
     return 0<= x <2*PI
 
-row_sum=[np.sum(matrix[k,:]) for k in range(L)]
-
-#set the gap between two consecutive ideograms
-gap=2*PI*0.005
-ideogram_length=2*PI*np.asarray(row_sum)/sum(row_sum)-gap*np.ones(L)
 
 def get_ideogram_ends(ideogram_len, gap):
-    ideo_ends=[]
+    ideo_ends=np.zeros([len(ideogram_len),2])
     left=0
     for k in range(len(ideogram_len)):
         right=left+ideogram_len[k]
-        ideo_ends.append([left, right])   
+        ideo_ends[k][0] = left
+        ideo_ends[k][1] = right   
         left=right+gap
     return ideo_ends 
 
-ideo_ends=get_ideogram_ends(ideogram_length, gap)
-ideo_ends
 def make_ideogram_arc(R, phi, a=50):
     # R is the circle radius
     # phi is the list of ends angle coordinates of an arc
     # a is a parameter that controls the number of points to be evaluated on an arc
-    if not test_2PI(phi[0]) or not test_2PI(phi[1]):
+    if not bounds(phi[0]) or not bounds(phi[1]):
         phi=[moduloAB(t, 0, 2*PI) for t in phi]
     length=(phi[1]-phi[0])% 2*PI 
     nr=5 if length<=PI/4 else int(a*length/PI)
@@ -64,27 +52,20 @@ def make_ideogram_arc(R, phi, a=50):
         theta=np.linspace(phi[0], phi[1], nr)
     return R*np.exp(1j*theta)   
 
-z=make_ideogram_arc(1.3, [11*PI/6, PI/17])
-print(z)
+#z=make_ideogram_arc(1.3, [11*PI/6, PI/17])
+#print(z)
 
-labels=['Emma', 'Isabella', 'Ava', 'Olivia', 'Sophia']
-ideo_colors=['rgba(244, 109, 67, 0.75)',
-             'rgba(253, 174, 97, 0.75)',
-             'rgba(254, 224, 139, 0.75)',
-             'rgba(217, 239, 139, 0.75)',
-             'rgba(166, 217, 106, 0.75)']#brewer colors with alpha set on 0.75
 
 def map_data(data_matrix, row_value, ideogram_length):
+    L, M=data_matrix.shape
     mapped=np.zeros(data_matrix.shape)
-    for j  in range(L):
-        mapped[:, j]=ideogram_length*data_matrix[:,j]/row_value
+    #for j  in range(L):
+    #    mapped[:, j]=ideogram_length*data_matrix[:,j]/row_value
+
+    mapped = ideogram_length * data_matrix / row_value
+
     return mapped 
 
-mapped_data=map_data(matrix, row_sum, ideogram_length)
-mapped_data
-
-idx_sort=np.argsort(mapped_data, axis=1)
-idx_sort
 
 def make_ribbon_ends(mapped_data, ideo_ends,  idx_sort):
     L=mapped_data.shape[0]
@@ -98,8 +79,6 @@ def make_ribbon_ends(mapped_data, ideo_ends,  idx_sort):
             start=ribbon_boundary[k][j]
     return [[(ribbon_boundary[k][j],ribbon_boundary[k][j+1] ) for j in range(L)] for k in range(L)]   
 
-ribbon_ends=make_ribbon_ends(mapped_data, ideo_ends,  idx_sort)
-print ('ribbon ends starting from the ideogram[2]\n', ribbon_ends[2])
 def control_pts(angle, radius):
     #angle is a  3-list containing angular coordinates of the control points b0, b1, b2
     #radius is the distance from b1 to the  origin O(0,0) 
@@ -108,9 +87,12 @@ def control_pts(angle, radius):
         raise InvalidInputError('angle must have len =3')
     b_cplx=np.array([np.exp(1j*angle[k]) for k in range(3)])
     b_cplx[1]=radius*b_cplx[1]
-    return list(zip(b_cplx.real, b_cplx.imag))[:]
+    if sys.version_info[0] < 3:
+        return zip(b_cplx.real, b_cplx.imag)
+    else:
+        return list(zip(b_cplx.real, b_cplx.imag))[:]
 def ctrl_rib_chords(l, r, radius):
-    # this function returns a 2-list containing control poligons of the two quadratic Bezier
+    # this function returns a 2-list containing control polygons of the two quadratic Bezier
     #curves that are opposite sides in a ribbon
     #l (r) the list of angular variables of the ribbon arc ends defining 
     #the ribbon starting (ending) arc 
@@ -119,16 +101,11 @@ def ctrl_rib_chords(l, r, radius):
         raise ValueError('the arc ends must be elements in a list of len 2')
     return [control_pts([l[j], (l[j]+r[j])/2, r[j]], radius) for j in range(2)]
 
-ribbon_color=[L*[ideo_colors[k]] for k in range(L)]
-ribbon_color[0][4]=ideo_colors[4]
-ribbon_color[1][2]=ideo_colors[2]
-ribbon_color[2][3]=ideo_colors[3]
-ribbon_color[2][4]=ideo_colors[4]
 
 def make_q_bezier(b):# defines the Plotly SVG path for a quadratic Bezier curve defined by the 
                      #list of its control points
     if len(b)!=3:
-        raise valueError('control poligon must have 3 points')
+        raise valueError('control polygon must have 3 points')
     A, B, C=b    
     return 'M '+str(A[0])+',' +str(A[1])+' '+'Q '+\
                 str(B[0])+', '+str(B[1])+ ' '+\
@@ -140,7 +117,7 @@ make_q_bezier(b)
 
 def make_ribbon_arc(theta0, theta1):
 
-    if test_2PI(theta0) and test_2PI(theta1):
+    if bounds(theta0) and bounds(theta1):
         if theta0 < theta1:
             theta0= moduloAB(theta0, -PI, PI)
             theta1= moduloAB(theta1, -PI, PI)
@@ -199,8 +176,8 @@ def make_ribbon(l, r, line_color, fill_color, radius=0.2):
     #l=[l[0], l[1]], r=[r[0], r[1]]  represent the opposite arcs in the ribbon 
     #line_color is the color of the shape boundary
     #fill_color is the fill color for the ribbon shape
-    poligon=ctrl_rib_chords(l,r, radius)
-    b,c =poligon  
+    polygon=ctrl_rib_chords(l,r, radius)
+    b,c =polygon  
            
     return  dict(
                 line=Line(
@@ -233,98 +210,134 @@ def invPerm(perm):
         inv[s] = i
     return inv
 
-layout=make_layout('Chord diagram', 400)
 radii_sribb=[0.4, 0.30, 0.35, 0.39, 0.12]# these value are set after a few trials 
 
-ribbon_info=[]
-for k in range(L):
-    
-    sigma=idx_sort[k]
-    sigma_inv=invPerm(sigma)
-    for j in range(k, L):
-        if matrix[k][j]==0 and matrix[j][k]==0: continue
-        eta=idx_sort[j]
-        eta_inv=invPerm(eta)
-        l=ribbon_ends[k][sigma_inv[j]]  
+def createChords(ends, colors, labels, row_sum, layout):
+    ideograms=[]
+
+    for k in range(len(ends)):
+        z= make_ideogram_arc(1.1, ends[k])
+        zi=make_ideogram_arc(1.0, ends[k])
+        m=len(z)
+        n=len(zi)
+        ideograms.append(Scatter(x=z.real,
+                                y=z.imag,
+                                mode='lines',
+                                line=Line(color=colors[k], shape='spline', width=0.25),
+                                text=labels[k]+'<br>'+'{:d}'.format(row_sum[k]), 
+                                hoverinfo='text'
+                                )
+                        )
         
-        if j==k:
-            layout['shapes'].append(make_self_rel(l, 'rgb(175,175,175)' ,
-                                    ideo_colors[k], radius=radii_sribb[k])) 
-            z=0.9*np.exp(1j*(l[0]+l[1])/2)
-            #the text below will be displayed when hovering the mouse over the ribbon
-            text=labels[k]+' commented on '+ '{:d}'.format(matrix[k][k])+' of '+ 'herself Fb posts',
-            ribbon_info.append(Scatter(x=[z.real],
-                                       y=[z.imag],
-                                       mode='markers',
-                                       marker=Marker(size=0.5, color=ideo_colors[k]),
-                                       text=text,
-                                       hoverinfo='text'
-                                       )
-                              )
-        else:
-            r=ribbon_ends[j][eta_inv[k]]
-            zi=0.9*np.exp(1j*(l[0]+l[1])/2)
-            zf=0.9*np.exp(1j*(r[0]+r[1])/2)
-            #texti and textf are the strings that will be displayed when hovering the mouse 
-            #over the two ribbon ends
-            texti=labels[k]+' commented on '+ '{:d}'.format(matrix[k][j])+' of '+\
-                  labels[j]+ ' Fb posts',
+        
+        path='M '
+        for s in range(m):
+            path+=str(z.real[s])+', '+str(z.imag[s])+' L '
             
-            textf=labels[j]+' commented on '+ '{:d}'.format(matrix[j][k])+' of '+\
-            labels[k]+ ' Fb posts',
-            ribbon_info.append(Scatter(x=[zi.real],
-                                       y=[zi.imag],
-                                       mode='markers',
-                                       marker=Marker(size=0.5, color=ribbon_color[k][j]),
-                                       text=texti,
-                                       hoverinfo='text'
-                                       )
-                              ),
-            ribbon_info.append(Scatter(x=[zf.real],
-                                       y=[zf.imag],
-                                       mode='markers',
-                                       marker=Marker(size=0.5, color=ribbon_color[k][j]),
-                                       text=textf,
-                                       hoverinfo='text'
-                                       )
-                              )
-            r=(r[1], r[0])#IMPORTANT!!!  Reverse these arc ends because otherwise you get
-                          # a twisted ribbon
-            #append the ribbon shape
-            layout['shapes'].append(make_ribbon(l, r, 'rgb(175,175,175)' , ribbon_color[k][j]))
+        Zi=np.array(zi.tolist()[::-1]) 
 
-ideograms=[]
-for k in range(len(ideo_ends)):
-    z= make_ideogram_arc(1.1, ideo_ends[k])
-    zi=make_ideogram_arc(1.0, ideo_ends[k])
-    m=len(z)
-    n=len(zi)
-    ideograms.append(Scatter(x=z.real,
-                             y=z.imag,
-                             mode='lines',
-                             line=Line(color=ideo_colors[k], shape='spline', width=0.25),
-                             text=labels[k]+'<br>'+'{:d}'.format(row_sum[k]), 
-                             hoverinfo='text'
-                             )
-                     )
+        for s in range(m):
+            path+=str(Zi.real[s])+', '+str(Zi.imag[s])+' L '
+        path+=str(z.real[0])+' ,'+str(z.imag[0]) 
     
-       
-    path='M '
-    for s in range(m):
-        path+=str(z.real[s])+', '+str(z.imag[s])+' L '
+        layout['shapes'].append(make_ideo_shape(path,'rgb(150,150,150)' , colors[k]))
+    return ideograms
+
+def createRibbons(matrix, labels, ribbon_ends, idx_sort, layout, ideo_colors, ribbon_color):
+
+
+    ribbon_info=[]
+    L, M=matrix.shape
+
+    for k in range(L):
         
-    Zi=np.array(zi.tolist()[::-1]) 
+        sigma=idx_sort[k]
+        sigma_inv=invPerm(sigma)
+        for j in range(k, L):
+            if matrix[k][j]==0 and matrix[j][k]==0: continue
+            eta=idx_sort[j]
+            eta_inv=invPerm(eta)
+            l=ribbon_ends[k][sigma_inv[j]]  
+            
+            if j==k:
+                layout['shapes'].append(make_self_rel(l, 'rgb(175,175,175)' ,
+                                        ideo_colors[k], radius=radii_sribb[k])) 
+                z=0.9*np.exp(1j*(l[0]+l[1])/2)
+                #the text below will be displayed when hovering the mouse over the ribbon
+                text=labels[k]+' commented on '+ '{:d}'.format(matrix[k][k])+' of '+ 'themselves Fb posts',
+                ribbon_info.append(Scatter(x=[z.real],
+                                        y=[z.imag],
+                                        mode='markers',
+                                        marker=Marker(size=0.5, color=ideo_colors[k]),
+                                        text=text,
+                                        hoverinfo='text'
+                                        )
+                                )
+            else:
+                r=ribbon_ends[j][eta_inv[k]]
+                zi=0.9*np.exp(1j*(l[0]+l[1])/2)
+                zf=0.9*np.exp(1j*(r[0]+r[1])/2)
+                #texti and textf are the strings that will be displayed when hovering the mouse 
+                #over the two ribbon ends
+                texti=labels[k]+' commented on '+ '{:d}'.format(matrix[k][j])+' of '+\
+                    labels[j]+ ' Fb posts',
+                
+                textf=labels[j]+' commented on '+ '{:d}'.format(matrix[j][k])+' of '+\
+                labels[k]+ ' Fb posts',
+                ribbon_info.append(Scatter(x=[zi.real],
+                                        y=[zi.imag],
+                                        mode='markers',
+                                        marker=Marker(size=0.5, color=ribbon_color[k][j]),
+                                        text=texti,
+                                        hoverinfo='text'
+                                        )
+                                ),
+                ribbon_info.append(Scatter(x=[zf.real],
+                                        y=[zf.imag],
+                                        mode='markers',
+                                        marker=Marker(size=0.5, color=ribbon_color[k][j]),
+                                        text=textf,
+                                        hoverinfo='text'
+                                        )
+                                )
+                r=(r[1], r[0])#IMPORTANT!!!  Reverse these arc ends because otherwise you get
+                            # a twisted ribbon
+                #append the ribbon shape
+                layout['shapes'].append(make_ribbon(l, r, 'rgb(175,175,175)' , ribbon_color[k][j]))
+    return ribbon_info
 
-    for s in range(m):
-        path+=str(Zi.real[s])+', '+str(Zi.imag[s])+' L '
-    path+=str(z.real[0])+' ,'+str(z.imag[0]) 
-   
-    layout['shapes'].append(make_ideo_shape(path,'rgb(150,150,150)' , ideo_colors[k]))
-    
-data = Data(ideograms+ribbon_info)
-fig = Figure(data=data, layout=layout)
+def chord(matrix, labels, ideo_colors):
 
-import plotly.offline as off
-off.init_notebook_mode()
+    L, M=matrix.shape
+    row_sum=np.sum(matrix, axis=1)#.tolist() #[np.sum(matrix[k,:]) for k in range(L)]
 
-off.iplot(fig, filename='chord-diagram-Fb') 
+    #set the gap between two consecutive ideograms
+    gap=2*PI*0.005
+    ideogram_length=2*PI*row_sum/sum(row_sum)-gap*np.ones(L)
+    mapped_data=map_data(matrix, row_sum, ideogram_length)
+    idx_sort=np.argsort(mapped_data, axis=1)
+
+    ideo_ends=get_ideogram_ends(ideogram_length, gap)
+    #ideo_ends
+
+    ribbon_ends=make_ribbon_ends(mapped_data, ideo_ends,  idx_sort)
+    #print ('ribbon ends starting from the ideogram[2]\n', ribbon_ends[2])
+
+    ribbon_color=[L*[ideo_colors[k]] for k in range(L)]
+    ribbon_color[0][4]=ideo_colors[4]
+    ribbon_color[1][2]=ideo_colors[2]
+    ribbon_color[2][3]=ideo_colors[3]
+    ribbon_color[2][4]=ideo_colors[4]
+
+    layout=make_layout('Chord diagram', 400)
+
+    ideograms=createChords(ideo_ends, ideo_colors, labels, row_sum, layout)
+    ribbon_info = createRibbons(matrix, labels, ribbon_ends, idx_sort, layout, ideo_colors, ribbon_color)    
+    data = Data(ideograms+ribbon_info)
+    fig = Figure(data=data, layout=layout)
+
+    import plotly.offline as off
+    off.init_notebook_mode()
+
+    #off.iplot(fig, filename='chord-diagram-Fb') 
+    return off, fig
